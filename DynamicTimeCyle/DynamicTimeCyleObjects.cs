2a49ab44cc31cabcf1e47363399bff8d1dbd999e
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2022-06-05 18:44:07 -0400

    Add files via upload

---
+++ DynamicTimeCyle/DynamicTimeCyleObjects.cs
@@ -0,0 +1,12 @@
+namespace r1ft.DynamicTimeCyle
+{
+    internal static class Objects
+    {
+        public static readonly string nextPhase = "Menu UI/UI/Matchmaker Location Selection/Conditions Panel/TimePhaseContainer/NextPhase";
+        public static readonly string currentPhase = "Menu UI/UI/Matchmaker Location Selection/Conditions Panel/TimePhaseContainer/CurrentPhase";
+        public static readonly string factoryNext = "Menu UI/UI/Matchmaker Location Selection/Conditions Panel Factory/TimePhaseContainer/NextPhase";
+        public static readonly string factoryCurrent = "Menu UI/UI/Matchmaker Location Selection/Conditions Panel Factory/TimePhaseContainer/CurrentPhase";
+
+        public static readonly string Label = "/Label";
+    }
+}
